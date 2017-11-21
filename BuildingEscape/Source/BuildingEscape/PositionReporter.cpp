c2f982e8d64936a39660bf5682c96b121e8eba91
MRLAPTONSSAN\Peter <p-meisel@hotmail.com>
2017-11-21 21:10:52 +0100

    no message

--- BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
+++ BuildingEscape/Source/BuildingEscape/PositionReporter.cpp
@@ -11,6 +11,7 @@ UPositionReporter::UPositionReporter()
 	PrimaryComponentTick.bCanEverTick = true;
 
 	// ...
+	
 }
 
 
@@ -20,7 +21,7 @@ void UPositionReporter::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
-	
+	UE_LOG(LogTemp, Warning, TEXT("Position reporter reporting for duty!"));
 }
 
 
